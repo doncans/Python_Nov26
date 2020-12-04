f2b6657b45d7cf5bd3efe0dc72d39e77fd6e4035
donkenaa <doncan.py@gmail.com>
2020-12-04 21:55:50 +0530

    updated

---
+++ fun.py
@@ -0,0 +1,10 @@
+def cal(a, b, operation):
+    if operation == '+':
+        print(a+b)
+    elif operation == '-':
+        print(a-b)
+    elif operation == '*':
+        print(a*b)
+    else:
+        print('Give the valid inputs')
+#cal(10,56, '*')
---
+++ fun2.py
@@ -0,0 +1,8 @@
+from fun import cal
+
+def caliculate(emp_sal):
+    return cal(emp_sal,0.1,'*')
+    
+    
+empl_sal = 1000 
+print(caliculate(empl_sal))
